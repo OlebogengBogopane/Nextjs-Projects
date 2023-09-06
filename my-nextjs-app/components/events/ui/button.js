42e8c921d500f16a839bf7694e29b76a505dc02b
OlebogengBogopane <125870308+OlebogengBogopane@users.noreply.github.com>
2023-09-06 15:16:19 +0200

    styling and dynamic routing

--- my-nextjs-app/components/events/ui/button.js
+++ my-nextjs-app/components/events/ui/button.js
@@ -3,10 +3,10 @@ import classes from './button.module.css'
 
 function Button(props) {
   return (
-    <Link href={props.link}>  
-    <a className={classes.btn}></a>{props.children}
-    </Link>
-  )
+    (<Link href={props.link} className={classes.btn}>
+      {props.children}
+    </Link>)
+  );
 };
 
 export default Button
